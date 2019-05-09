339a05e77cfe25ad7c7b1a710b098c5f4088c8f4
jd-13 <jackd8989@gmail.com>
2019-05-09 18:50:35 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="SpyderWeb",
+    version='0.1.0',
+    description="",
+    #long_description=long_description,
+    url="",
+    author="Jack Devlin",
+    author_email="jack@jdevlin.me",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords="",
+    packages=["SpyderWeb"],
+    install_requires=[],
+)
